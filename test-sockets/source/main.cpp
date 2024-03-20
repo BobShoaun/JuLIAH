56439d61b2940cb3c00a12d7b967fb72e397d4f6
dianzrong <dianrong2012@gmail.com>
2024-03-20 17:19:14 -0400

    clean up and disconnect

--- test-sockets/source/main.cpp
+++ test-sockets/source/main.cpp
@@ -20,9 +20,6 @@
 #include "mbed-trace/mbed_trace.h"
 
 // // MQTT stuff
-// #include "MQTTNetwork.h"
-// #include "MQTTmbed.h"
-// #include "MQTTClient.h"
 int arrivedcount = 0;
 
 // newest MQTT stuff
@@ -127,17 +124,15 @@ public:
         MQTTPacket_connectData data = MQTTPacket_connectData_initializer;
         data.MQTTVersion = 3;
         data.clientID.cstring = "mbed-sample";
-        // data.username.cstring = "testuser";
-        // data.password.cstring = "testpassword";
         if ((rc = client.connect(data)) != 0)
             printf("rc from client connect is %d\r\n", rc);
 
-        if ((rc = client.subscribe(topicName, MQTT::QOS1, messageArrived)) != 0)
+        if ((rc = client.subscribe(topicName, MQTT::QOS0, messageArrived)) != 0)
             printf("rc from MQTT subscribe is %d\r\n", rc);
 
 
         char buf[100];
-        sprintf(buf, "Hello World!  QoS 1 message from\r\n");
+        sprintf(buf, "Hello World!  QoS 1 message from group 8173 \r\n");
         message.qos = MQTT::QOS1;
         message.retained = false;
         message.dup = false;
@@ -150,56 +145,17 @@ public:
         while (arrivedcount < 1)
             client.yield(100);
 
-        // end of MQTT stuff
-
-        /* opening the socket only allocates resources */
-        result = _socket.open(_net);
-        if (result != 0) {
-            printf("Error! _socket.open() returned: %d\r\n", result);
-            return;
-        }
-
-#if MBED_CONF_APP_USE_TLS_SOCKET
-        result = _socket.set_root_ca_cert(root_ca_cert);
-        if (result != NSAPI_ERROR_OK) {
-            printf("Error: _socket.set_root_ca_cert() returned %d\n", result);
-            return;
-        }
-        _socket.set_hostname(MBED_CONF_APP_HOSTNAME);
-#endif // MBED_CONF_APP_USE_TLS_SOCKET
-
-        /* now we have to find where to connect */
-
-        SocketAddress address;
-
-        if (!resolve_hostname(address)) {
-            return;
-        }
-
-        address.set_port(REMOTE_PORT);
-
-        /* we are connected to the network but since we're using a connection oriented
-         * protocol we still need to open a connection on the socket */
-
-        printf("Opening connection to remote port %d\r\n", REMOTE_PORT);
-
-        result = _socket.connect(address);
-        if (result != 0) {
-            printf("Error! _socket.connect() returned: %d\r\n", result);
-            return;
-        }
-
-        /* exchange an HTTP request and response */
-
-        if (!send_http_request()) {
-            return;
-        }
-
-        if (!receive_http_response()) {
-            return;
-        }
-
         printf("Demo concluded successfully \r\n");
+
+        // clean up
+        rc = client.unsubscribe(topicName);
+        printf("rc from unsubscribe was %d\r\n", rc);
+
+        rc = client.disconnect();
+        printf("rc from client disconnect was %d\r\n", rc);
+
+        _net->disconnect();
+        printf("disconnected from wifi");
     }
 
 private:
@@ -216,62 +172,6 @@ private:
         }
 
         printf("%s address is %s\r\n", hostname, (address.get_ip_address() ? address.get_ip_address() : "None") );
-
-        return true;
-    }
-
-    bool send_http_request()
-    {
-        /* loop until whole request sent */
-        const char buffer[] = "GET / HTTP/1.1\r\n"
-                              "Host: ifconfig.io\r\n"
-                              "Connection: close\r\n"
-                              "\r\n";
-
-        nsapi_size_t bytes_to_send = strlen(buffer);
-        nsapi_size_or_error_t bytes_sent = 0;
-
-        printf("\r\nSending message: \r\n%s", buffer);
-
-        while (bytes_to_send) {
-            bytes_sent = _socket.send(buffer + bytes_sent, bytes_to_send);
-            if (bytes_sent < 0) {
-                printf("Error! _socket.send() returned: %d\r\n", bytes_sent);
-                return false;
-            } else {
-                printf("sent %d bytes\r\n", bytes_sent);
-            }
-
-            bytes_to_send -= bytes_sent;
-        }
-
-        printf("Complete message sent\r\n");
-
-        return true;
-    }
-
-    bool receive_http_response()
-    {
-        char buffer[MAX_MESSAGE_RECEIVED_LENGTH];
-        int remaining_bytes = MAX_MESSAGE_RECEIVED_LENGTH;
-        int received_bytes = 0;
-
-        /* loop until there is nothing received or we've ran out of buffer space */
-        nsapi_size_or_error_t result = remaining_bytes;
-        while (result > 0 && remaining_bytes > 0) {
-            result = _socket.recv(buffer + received_bytes, remaining_bytes);
-            if (result < 0) {
-                printf("Error! _socket.recv() returned: %d\r\n", result);
-                return false;
-            }
-
-            received_bytes += result;
-            remaining_bytes -= result;
-        }
-
-        /* the message is likely larger but we only want the HTTP response code */
-
-        printf("received %d bytes:\r\n%.*s\r\n\r\n", received_bytes, strstr(buffer, "\n") - buffer, buffer);
 
         return true;
     }
@@ -316,12 +216,6 @@ private:
 
 private:
     NetworkInterface *_net;
-
-#if MBED_CONF_APP_USE_TLS_SOCKET
-    TLSSocket _socket;
-#else
-    TCPSocket _socket;
-#endif // MBED_CONF_APP_USE_TLS_SOCKET
 };
 
 int main() {
